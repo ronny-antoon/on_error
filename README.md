31bf50a47bfd63406d5845092ab7c433b89e0e7e
ronny-antoon <90724911+ronny-antoon@users.noreply.github.com>
2024-06-04 13:43:28 +0300

    v0.1.0

--- README.md
+++ README.md
@@ -1,13 +1,58 @@
-# Test Component
+# on_error Component
 
-This is a simple example component.
+## Description
+
+The `on_error` component provides macros for handling errors and logging error messages in ESP32 applications.
 
 ## Installation
 
-- step 1
-- step 2
+### Installation
 
-## Getting Started
+To use the `on_error` component framework in your ESP-IDF project, follow these steps:
 
-- step 1
-- step 2
+1. Clone or download the `on_error` repository.
+2. Copy the `on_error` directory into your ESP-IDF project's `components` directory.
+3. Configure the component in your `CMakeLists.txt` file.
+
+OR
+
+- Add dependencies to idf_component.yml in your project/main directory
+```
+dependencies:
+  ronny-antoon/on_error:
+    version: "^0.1.0"
+```
+## Usage
+
+The `on_error` component provides the following macros:
+
+- `ON_ERR_PRINT(esp_err, format, ...)`: Prints an error message if an ESP error occurs.
+- `ON_ERR_PRINT_RETURN(esp_err, ret, format, ...)`: Prints an error message if an ESP error occurs and returns a value.
+- `ON_NULL_PRINT(ptr, format, ...)`: Prints an error message if a pointer is null.
+- `ON_NULL_PRINT_RETURN(ptr, ret, format, ...)`: Prints an error message if a pointer is null and returns a value.
+
+### Example
+
+```cpp
+#include "on_error.hpp"
+
+void example_function() {
+    esp_err_t err = some_function();
+    ON_ERR_PRINT_RETURN(err, ESP_FAIL, "Failed to perform some_function()");
+    
+    SomeType* ptr = get_pointer();
+    ON_NULL_PRINT_RETURN(ptr, NULL, "Pointer is NULL");
+
+    // Continue with the rest of the function
+}
+```
+
+## License
+
+The `metric_tracker` component framework is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
+
+## Links
+
+- [GitHub Repository](https://github.com/ronny-antoon/on_error)
+- [Espressif IDF Registry](https://components.espressif.com/components/ronny-antoon/on_error)
+
--- include/on_error.hpp
+++ include/on_error.hpp
@@ -4,6 +4,13 @@
 #include <esp_err.h>
 #include <esp_log.h>
 
+/**
+ * @brief Macro to print an error message if an ESP error occurs.
+ *
+ * @param esp_err The ESP error code.
+ * @param format The format string for the error message.
+ * @param ... Additional arguments for the format string.
+ */
 #define ON_ERR_PRINT(esp_err, format, ...)                                 \
     do                                                                     \
     {                                                                      \
@@ -13,6 +20,15 @@
         }                                                                  \
     } while (0)
 
+/**
+ * @brief Macro to print an error message if an ESP error occurs and return a value.
+ *
+ * @param esp_err The ESP error code.
+ * @param ret The value to return if an error occurs.
+ * @param format The format string for the error message.
+ * @param ... Additional arguments for the format string.
+ * @return ret The value to return if an error occurs.
+ */
 #define ON_ERR_PRINT_RETURN(esp_err, ret, format, ...)                     \
     do                                                                     \
     {                                                                      \
@@ -23,6 +39,13 @@
         }                                                                  \
     } while (0)
 
+/**
+ * @brief Macro to print an error message if a pointer is null.
+ *
+ * @param ptr The pointer to check.
+ * @param format The format string for the error message.
+ * @param ... Additional arguments for the format string.
+ */
 #define ON_NULL_PRINT(ptr, format, ...)                                    \
     do                                                                     \
     {                                                                      \
@@ -32,6 +55,15 @@
         }                                                                  \
     } while (0)
 
+/**
+ * @brief Macro to print an error message if a pointer is null and return a value.
+ *
+ * @param ptr The pointer to check.
+ * @param ret The value to return if the pointer is null.
+ * @param format The format string for the error message.
+ * @param ... Additional arguments for the format string.
+ * @return ret The value to return if the pointer is null.
+ */
 #define ON_NULL_PRINT_RETURN(ptr, ret, format, ...)                        \
     do                                                                     \
     {                                                                      \
