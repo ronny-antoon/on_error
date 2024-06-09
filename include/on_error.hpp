3bee5f52fb0f7179ab020d8cf1475bf5f1bb17a2
ronny-antoon <90724911+ronny-antoon@users.noreply.github.com>
2024-06-10 02:46:12 +0300

    formating change

--- include/on_error.hpp
+++ include/on_error.hpp
@@ -11,14 +11,12 @@
  * @param format The format string for the error message.
  * @param ... Additional arguments for the format string.
  */
-#define ON_ERR_PRINT(esp_err, format, ...)                                 \
-    do                                                                     \
-    {                                                                      \
-        if (esp_err != ESP_OK)                                             \
-        {                                                                  \
-            ESP_LOGE(__FILE__, "[%d] - " format, __LINE__, ##__VA_ARGS__); \
-        }                                                                  \
-    } while (0)
+#define ON_ERR_PRINT(esp_err, format, ...)                           \
+  do {                                                               \
+    if (esp_err != ESP_OK) {                                         \
+      ESP_LOGE(__FILE__, "[%d] - " format, __LINE__, ##__VA_ARGS__); \
+    }                                                                \
+  } while (0)
 
 /**
  * @brief Macro to print an error message if an ESP error occurs and return a value.
@@ -29,15 +27,13 @@
  * @param ... Additional arguments for the format string.
  * @return ret The value to return if an error occurs.
  */
-#define ON_ERR_PRINT_RETURN(esp_err, ret, format, ...)                     \
-    do                                                                     \
-    {                                                                      \
-        if (esp_err != ESP_OK)                                             \
-        {                                                                  \
-            ESP_LOGE(__FILE__, "[%d] - " format, __LINE__, ##__VA_ARGS__); \
-            return ret;                                                    \
-        }                                                                  \
-    } while (0)
+#define ON_ERR_PRINT_RETURN(esp_err, ret, format, ...)               \
+  do {                                                               \
+    if (esp_err != ESP_OK) {                                         \
+      ESP_LOGE(__FILE__, "[%d] - " format, __LINE__, ##__VA_ARGS__); \
+      return ret;                                                    \
+    }                                                                \
+  } while (0)
 
 /**
  * @brief Macro to print an error message if a pointer is null.
@@ -46,14 +42,12 @@
  * @param format The format string for the error message.
  * @param ... Additional arguments for the format string.
  */
-#define ON_NULL_PRINT(ptr, format, ...)                                    \
-    do                                                                     \
-    {                                                                      \
-        if (ptr == NULL)                                                   \
-        {                                                                  \
-            ESP_LOGE(__FILE__, "[%d] - " format, __LINE__, ##__VA_ARGS__); \
-        }                                                                  \
-    } while (0)
+#define ON_NULL_PRINT(ptr, format, ...)                              \
+  do {                                                               \
+    if (ptr == NULL) {                                               \
+      ESP_LOGE(__FILE__, "[%d] - " format, __LINE__, ##__VA_ARGS__); \
+    }                                                                \
+  } while (0)
 
 /**
  * @brief Macro to print an error message if a pointer is null and return a value.
@@ -64,14 +58,12 @@
  * @param ... Additional arguments for the format string.
  * @return ret The value to return if the pointer is null.
  */
-#define ON_NULL_PRINT_RETURN(ptr, ret, format, ...)                        \
-    do                                                                     \
-    {                                                                      \
-        if (ptr == nullptr)                                                \
-        {                                                                  \
-            ESP_LOGE(__FILE__, "[%d] - " format, __LINE__, ##__VA_ARGS__); \
-            return ret;                                                    \
-        }                                                                  \
-    } while (0)
+#define ON_NULL_PRINT_RETURN(ptr, ret, format, ...)                  \
+  do {                                                               \
+    if (ptr == nullptr) {                                            \
+      ESP_LOGE(__FILE__, "[%d] - " format, __LINE__, ##__VA_ARGS__); \
+      return ret;                                                    \
+    }                                                                \
+  } while (0)
 
-#endif // ON_ERROR_HPP
+#endif  // ON_ERROR_HPP
